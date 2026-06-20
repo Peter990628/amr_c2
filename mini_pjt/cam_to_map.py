68c10bbaa0141a307c33d4a9a1b1b443fd901989
mjdep0 <136634878+mjdep0@users.noreply.github.com>
2026-06-20 22:47:59 +0900

    Add files via upload

---
+++ mini_pjt/cam_to_map.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.duration import Duration
+
+from geometry_msgs.msg import PointStamped
+from tf2_ros import Buffer, TransformListener
+from tf2_geometry_msgs.tf2_geometry_msgs import do_transform_point
+from rclpy import Time
+
+class Camtomap(Node):
+
+    def __init__(self):
+        super().__init__('cam_to_map')
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        self.pub = self.create_publisher(PointStamped,'/rc_pos', 10)
+
+        self.sub = self.create_subscription(PointStamped, 'center_true', self.rc_callback, 10)
+
+    def rc_callback(self, msg):
+        try:
+              pt_map = self.tf_buffer.transform(msg, 'map', timeout=Duration(seconds=1.0))
+              self.pub.publish(pt_map)
+
+              self.get_logger().info(f"map: x={pt_map.point.x:.2f}, y={pt_map.point.y:.2f}, z={pt_map.point.z:.2f}")
+        except Exception as e:
+              self.get_logger().warn(f"{e}")
+
+    
+def main():
+	rclpy.init()
+	node = Camtomap()
+	rclpy.spin(node)
+	node.destroy_node()
+	rclpy.shutdown()
+	
+if __name__ == '__main__':
+	main()
