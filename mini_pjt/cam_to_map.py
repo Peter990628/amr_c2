c1143e89f7a04b46dc5dbd593419f6a3f058b0a3
mjdep0 <136634878+mjdep0@users.noreply.github.com>
2026-06-21 12:06:40 +0900

    Update cam_to_map.py

--- mini_pjt/cam_to_map.py
+++ mini_pjt/cam_to_map.py
@@ -15,7 +15,7 @@ class Camtomap(Node):
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self.pub = self.create_publisher(PointStamped,'/rc_pos', 10)
+        self.pub = self.create_publisher(PointStamped,'rc_pos', 10)
 
         self.sub = self.create_subscription(PointStamped, 'center_true', self.rc_callback, 10)
 
